dd5a560506a390de3dd3c86d0ab9a607422d185e
aymfx <aymfx@qq.com>
2024-01-02 23:26:17 +0800

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential',"@vue/eslint-config-typescript"],
+
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 0,
+  },
+};
